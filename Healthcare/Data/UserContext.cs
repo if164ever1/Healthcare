2928e8f7261edb7b57b37a029ecb26b9dbe1e784
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2021-10-21 14:34:50 +0300

    added registration form layout

---
+++ Healthcare/Data/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Healthcare.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Healthcare.Data
+{
+    public class UserContext: DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options): base(options)
+        { }
+
+        public DbSet<UserModel> Users { get; set; }
+
+    }
+}
